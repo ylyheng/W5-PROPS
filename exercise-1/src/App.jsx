b6d8ee8e0846cea3ee62ee0a0bb4bcc71be6f855
Onion <mchess563@gmail.com>
2026-02-12 14:09:55 +0700

    finish exercise1

--- exercise-1/src/App.jsx
+++ exercise-1/src/App.jsx
@@ -10,8 +10,8 @@ export const userData = {
 export function User() {
   return (
     <div id="user" data-testid="user">
-      <h2>TODO: FIRST NAME LAST NAME</h2>
-      <p>TODO: TITLE</p>
+      <h2>{userData.firstName} {userData.lastName}</h2>
+      <p>{userData.title}</p>
     </div>
   );
 }
